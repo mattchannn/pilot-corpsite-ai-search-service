c37042c431c788804c47c92c8fb1e8f0240fdc9d
Mat <klchan.358@gmail.com>
2026-03-06 15:05:27 +0800

    feat: finalize controller for super basic function

--- src/main/java/com/pilot/corpsite/controller/SearchController.java
+++ src/main/java/com/pilot/corpsite/controller/SearchController.java
@@ -1,16 +1,34 @@
 package com.pilot.corpsite.controller;
 
+import com.pilot.corpsite.model.api.request.SearchRequest;
 import com.pilot.corpsite.model.api.response.SearchResult;
+import com.pilot.corpsite.service.GenerateAISummary;
+import com.pilot.corpsite.service.GetSearchResult;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class SearchController {
+    private final GetSearchResult getSearchResult;
+
+    private final GenerateAISummary generateAISummary;
+
+    public SearchController(GetSearchResult getSearchResult, GenerateAISummary generateAISummary) {
+        this.getSearchResult = getSearchResult;
+        this.generateAISummary = generateAISummary;
+    }
+
     @PostMapping(value = "/search", produces = "application/json")
-    public ResponseEntity<SearchResult> search() {
+    public ResponseEntity<SearchResult> search(@RequestBody SearchRequest request) {
         SearchResult resp = new SearchResult();
+        List<String> chunks = this.getSearchResult.search(request.getQuery());
+        String summary = this.generateAISummary.execute(request.getQuery(), chunks);
+        resp.setSummary(summary);
         return new ResponseEntity<>(resp, HttpStatus.OK);
     }
 }
